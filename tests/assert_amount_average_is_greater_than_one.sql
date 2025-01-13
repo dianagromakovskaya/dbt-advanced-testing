3b2304629213fc310fb6ed2b5b4e327c70fff507
Diana Gromakovskaia <dianagr1996@gmail.com>
2025-01-13 14:32:49 +0000

    added severity for the tests

--- tests/assert_amount_average_is_greater_than_one.sql
+++ tests/assert_amount_average_is_greater_than_one.sql
@@ -1,8 +1,8 @@
-{{ config(enabled = false) }}
+{{ config(severity = 'warn') }}
 
 select
     customer_id, 
     avg(amount) as average_amount
-from {{ ref('stg_orders') }}
+from {{ ref('fct_orders') }}
 group by 1
 having count(customer_id) > 1 and average_amount < 1
